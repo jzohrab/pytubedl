983c3b6fd32aa9409ba7e1f19c39d80e9006831f
Jeff Zohrab <jzohrab@gmail.com>
2022-04-22 14:06:42 -0700

    wip cleanup

--- pytubedl/p2.py
+++ pytubedl/p2.py
@@ -2,16 +2,24 @@ from tkinter import *
 from tkinter import filedialog
 from pygame import mixer
 from mutagen.mp3 import MP3
-
+from enum import Enum
 import tkinter.ttk as ttk
 
 class MusicPlayer:
+
+    class State(Enum):
+        NEW = 0
+        LOADED = 1
+        PLAYING = 2
+        PAUSED = 3
 
     def __init__(self, window):
         window.title('MP3 Player')
         window.geometry('600x400')
 
         mixer.init()
+
+        self.state = MusicPlayer.State.NEW
 
         self.music_file = None
         self.song_length_ms = 0
@@ -67,7 +75,7 @@ class MusicPlayer:
         value_ms_f = float(self.slider.get())
         mixer.music.play(loops = 0, start = (value_ms_f / 1000.0))
         self.start_pos_ms = value_ms_f
-        if not self.is_playing:
+        if self.state is MusicPlayer.State.PAUSED:
             mixer.music.pause()
         self.update_slider()
 
@@ -76,12 +84,10 @@ class MusicPlayer:
             self.slider.after_cancel(self.slider_update_id)
 
     def update_slider(self):
-        if not self.is_playing:
-            return
-
-        current_pos_ms = mixer.music.get_pos()
-        self.slider.set(self.start_pos_ms + current_pos_ms)
-        self.slider_update_id = self.slider.after(500, self.update_slider)
+        if self.state is MusicPlayer.State.PLAYING:
+            current_pos_ms = mixer.music.get_pos()
+            self.slider.set(self.start_pos_ms + current_pos_ms)
+            self.slider_update_id = self.slider.after(50, self.update_slider)
 
     def load(self):
         f = filedialog.askopenfilename()
@@ -98,34 +104,40 @@ class MusicPlayer:
         self.slider.config(to = self.song_length_ms, value=0)
         self.start_pos_ms = 0.0
         self.play_started = False
+        self.state = MusicPlayer.State.LOADED
 
     def play(self):
         self.cancel_slider_updates()
         if self.music_file is None:
             return
 
-        if not self.play_started:
+        if self.state is MusicPlayer.State.LOADED:
             # First play, load and start.
             self.play_started = True
             self.play_button.configure(text = 'Pause')
             mixer.music.load(self.music_file)
             mixer.music.play()
+            self.state = MusicPlayer.State.PLAYING
             self.is_playing = True
             self.start_pos_ms = 0
             self.update_slider()
 
-        elif self.is_playing:
-            # Play had already started, now we're pausing.
+        elif self.state is MusicPlayer.State.PLAYING:
             mixer.music.pause()
             self.cancel_slider_updates()
+            self.state = MusicPlayer.State.PAUSED
             self.is_playing = False
             self.play_button.configure(text = 'Play')
 
-        else:
+        elif self.state is MusicPlayer.State.PAUSED:
             mixer.music.unpause()
-            self.is_playing = True
+            self.state = MusicPlayer.State.PLAYING
             self.update_slider()
             self.play_button.configure(text = 'Pause')
+
+        else:
+            # Should never get here, but in case I missed something ...
+            raise RuntimeError('??? weird state?')
 
     def stop(self):
         mixer.music.stop()
